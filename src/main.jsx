94ad580cdd89ae24304c72f41fe0ba0ea4b325df
Shanto Noor <shantonoor900@gmail.com>
2024-03-16 16:07:52 +0600

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react-refresh/only-export-components */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./main.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <h1>Hello</h1>
+  </React.StrictMode>
+);
