2399cfd7253a14b204c3ead19af3a8c674add445
Shanto Noor <shantonoor900@gmail.com>
2024-03-19 03:34:21 +0600

    deployment successfull

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosn = axios.create({
   baseURL: import.meta.env.PROD
-    ? "https://MedCamp-shanto-noors-projects.vercel.app"
+    ? "https://med-camp.vercel.app/"
     : "http://localhost:3000",
 });
